ade3d70b469bded25e98d81529ed6c4d784a9bde
euonymus <euonymus0220@gmail.com>
2018-06-22 21:05:52 +0900

    try

--- src/containers/gluon_list.js
+++ src/containers/gluon_list.js
@@ -35,27 +35,30 @@ class GluonList extends Component {
 		return '';
 	    }
 	    return (
-           <div key={gluon.id}>
-               <h2>{this.props.quark_property.caption_ja}</h2>
-               <div className="related" >
-                   <div className="well subject-relation white">
+		<div key={gluon.id}>
                     {(() => {
                         if (gluon.id != first.id)
                         return <hr />;
                     })()}
                     <Gluon gluon = {gluon} />
-                   </div>
-               </div>
-	   </div>
+		</div>
 	    );
 	});
     }
 
     render () {
+	if (this.props.quark_property.gluons.length == 0) {
+	    return '';
+	}
 	return (
            <div>
-              {this.renderGluons()}
-           </div>
+               <h2>{this.props.quark_property.caption_ja}</h2>
+               <div className="related" >
+                   <div className="well subject-relation white">
+                       {this.renderGluons()}
+                   </div>
+               </div>
+	   </div>
 	)
     }
 }
