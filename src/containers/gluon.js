59ae143d1f7885c72837f6b85761aa6c11751107
euonymus <euonymus0220@gmail.com>
2018-06-29 12:15:11 +0900

    chore

--- src/containers/gluon.js
+++ src/containers/gluon.js
@@ -17,12 +17,21 @@ class Gluon extends Component {
 	return res;
     }
 
+    renderGluonEdits() {
+	return (
+	   <span>
+<a href="/relations/edit/c86a8d45-70a5-4d26-95fb-39246d98bc66" className="glyphicon glyphicon glyphicon-pencil"></a>                         <form name="post_5b35a29c8e92d208113568" style={{display:"none"}} method="post" action="/relations/delete/c86a8d45-70a5-4d26-95fb-39246d98bc66"><input type="hidden" name="_method" value="POST"/></form><a href="#" className="glyphicon glyphicon-remove-sign" onClick="if (confirm(&quot;Are you sure you want to delete?&quot;)) { document.post_5b35a29c8e92d208113568.submit(); } event.returnValue = false; return false;"></a>
+           </span>
+	)
+    }
+
     relationText() {
 	if (this.props.current_quark.id == this.props.gluon.active_id) {
             return (
                <h4 className="media-heading">
                   {this.props.gluon.active.name} は
                   <Link to={`/subjects/relations/${this.gluedQuark().name}`}>{this.gluedQuark().name}</Link> {this.props.gluon.relation} {this.props.gluon.suffix}
+                  {this.renderGluonEdits()}
                </h4>
 	    );
 	} else if (this.props.current_quark.id == this.props.gluon.passive_id) {
