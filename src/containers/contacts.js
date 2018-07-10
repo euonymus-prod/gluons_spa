6a70d3b6bd133ffe29d5c37563168e7d9c272e3b
euonymus <euonymus0220@gmail.com>
2018-07-10 23:05:01 +0900

    contact form

--- src/containers/contacts.js
+++ src/containers/contacts.js
@@ -1,16 +1,83 @@
 // general
 // react
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 // redux
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 // component
 import GlobalFooter from '../components/global_footer';
 import Navbar from './navbar';
 
 
+const validate = values => {
+  const errors = {}
+  if (!values.name) {
+    errors.name = 'Required'
+  } else if (values.name.length > 255) {
+    errors.name = 'Must be less than 255'
+  }
+  if (!values.organization) {
+    errors.organization = 'Required'
+  } else if (values.organization.length > 255) {
+    errors.organization = 'Must be less than 255'
+  }
+  if (!values.department) {
+    errors.department = 'Required'
+  } else if (values.department.length > 255) {
+    errors.department = 'Must be less than 255'
+  }
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (values.email.length > 255) {
+    errors.email = 'Must be less than 255'
+  }
+  if (!values.body) {
+    errors.body = 'Required'
+  } else if (values.body.length > 2000) {
+    errors.body = 'Must be less than 255'
+  }
+  return errors
+}
+
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div className="input text">
+     <label htmlFor={input.id}>{label}</label>
+     {/*  required="required" maxLength="255" id="name" */ }
+     <input {...input} placeholder={label} type={type} className="form-control" />
+     {touched && (error && <span className="validation-error">{error}</span>)}
+  </div>
+)
+const renderTextarea = ({ input, label, type, meta: { touched, error } }) => (
+  <div className="input text">
+     <label htmlFor={input.id}>{label}</label>
+     {/*  required="required" maxLength="255" id="name" */ }
+     <textarea {...input} placeholder={label} type={type} className="form-control" />
+     {touched && (error && <span className="validation-error">{error}</span>)}
+  </div>
+)
+const renderSelect = ({ input, label, type, meta: { touched, error } }) => (
+  <div className="input select">
+     <label htmlFor={input.id}>{label}</label>
+     <Field name="topic" id="topic" component="select">
+        <option value="">(Select one topic)</option>
+        <option value="About Service">About Service</option>
+        <option value="Business relationship">Business relationship</option>
+        <option value="Media coverage">Media coverage</option>
+        <option value="Other">Other</option>
+     </Field>
+     {touched && (error && <span className="validation-error"><br />{error}</span>)}
+  </div>
+    )
+
 class Contacts extends Component {
+    onSubmit = (values) => {
+	console.log(values)
+	// this.props.addQuark(values);
+    }
+
  render () {
+  const { handleSubmit } = this.props;
   return (
    <div>
       <Navbar withSearchBar='1' />
@@ -21,37 +88,20 @@ class Contacts extends Component {
             <img src="/img/logo.gif" />
             <p>Leverage your knowledge by seeking relations among things, people, ETC. If you’d like to know more about how we can help you, put down anything here.</p>
 
-            <form method="post" acceptCharset="utf-8" className="search_top" action="/contacts">
-               <div style={{display:'none'}}><input type="hidden" name="_method" value="POST"/></div>
+            <form onSubmit={handleSubmit(this.onSubmit)} acceptCharset="utf-8" className="search_top">
                <div className="form-group center-block input-container-top">
-                  <div className="input text required">
-                     <label htmlFor="name">Name</label>
-                     <input type="text" name="name" className="form-control" required="required" id="name"/>
-                  </div>
-                  <div className="input text required">
-                     <label htmlFor="organization">Organization</label>
-                     <input type="text" name="organization" className="form-control" required="required" id="organization"/>
-                  </div>
-                  <div className="input text required">
-                     <label htmlFor="department">Department</label>
-                     <input type="text" name="department" className="form-control" required="required" id="department"/>
-                  </div>
-                  <div className="input email required">
-                     <label htmlFor="email">Email</label>
-                     <input type="email" name="email" className="form-control" required="required" id="email"/>
-                  </div>
+                  <Field name="name" component={renderField} type="text" id="name" label="Name" />
+
+                  <Field name="organization" component={renderField} type="text" id="organization" label="Organization" />
+
+                  <Field name="department" component={renderField} type="text" id="department" label="Department" />
+      
+                  <Field name="email" component={renderField} type="email" id="email" label="Email" />
+      
                   <br />
-                  <select name="topic" required="required">
-                     <option value="">(Select one topic)</option>
-                     <option value="About Service">About Service</option>
-                     <option value="Business relationship">Business relationship</option>
-                     <option value="Media coverage">Media coverage</option>
-                     <option value="Other">Other</option>
-                  </select>
-                  <div className="input textarea required">
-                     <label htmlFor="body">Body</label>
-                     <textarea name="body" className="form-control" required="required" id="body" rows="5"></textarea>
-                  </div>
+                  <Field name="topic" component={renderSelect} type="select" id="topic" label="Topic" />
+
+                  <Field name="body" component={renderTextarea} type="textarea" id="body" rows="5" label="Body" />
                </div>
                <button className="btn btn-primary" type="submit">Contact us</button>
             </form>
@@ -64,4 +114,7 @@ class Contacts extends Component {
   )
  }
 }
-export default connect(state => state)(Contacts);
+export default  reduxForm({
+  form: 'contact_form',
+  validate,
+})(withRouter(connect(state => state, { })(Contacts)));
